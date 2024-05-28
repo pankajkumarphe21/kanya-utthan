2b66200d1f5b28bf25bcce7d24d07032b97294f6
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-05-28 12:46:33 +0530

    final

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import AllRegistrations from './adminpanel/registrations/AllRegistrations';
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <AllRegistrations/>
-    </BrowserRouter>
+
     </>
   )
 }
