98a41f12c71b84da7f959d968a4d8df1c056a1c6
Pankaj Kumar <103842369+pankajkumarphe21@users.noreply.github.com>
2024-06-06 22:32:26 +0530

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignIn from "./Components/signin.jsx";
-import SignUp from "./Components/signup.jsx";
+import SignIn from "./components/signin.jsx";
+import SignUp from "./components/signup.jsx";
 import AllRequests from "./adminpanel/requests/AllRequests.jsx";
 import ApproveRequests from "./adminpanel/requests/ApproveRequests.jsx";
 import AllRegistrations from "./adminpanel/registrations/AllRegistrations.jsx";
 import ApproveRegistrations from "./adminpanel/registrations/ApproveRegistrations.jsx";
-import Home from "./Components/homepage/Home.jsx";
+import Home from "./components/homepage/Home.jsx";
 
 function App() {
   return (
