6a0df61da1a3440590c18770a9e99893506b8449
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-06 22:21:25 +0530

    home page modified

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import SignIn from "./Components/signin.jsx";
-// import SignUp from "./Components/signup.jsx";
+import SignIn from "./Components/signin.jsx";
+import SignUp from "./Components/signup.jsx";
 import AllRequests from "./adminpanel/requests/AllRequests.jsx";
 import ApproveRequests from "./adminpanel/requests/ApproveRequests.jsx";
 import AllRegistrations from "./adminpanel/registrations/AllRegistrations.jsx";
 import ApproveRegistrations from "./adminpanel/registrations/ApproveRegistrations.jsx";
-import Home from "./components/homepage/Home.jsx";
+import Home from "./Components/homepage/Home.jsx";
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/sign-in" element={<SignIn />} /> */}
-          {/* <Route path="/sign-up" element={<SignUp />} /> */}
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/registrations" element={<AllRegistrations />} />
           <Route
             path="/edit/:i"
--- public/src/components/homepage/Home.jsx
+++ public/src/components/homepage/Home.jsx
@@ -3,8 +3,24 @@ import RequestCard from './RequestCard'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
+      <div className='absolute top-0 h-[340px]'>
+        <img className='h-[340px] w-screen' src="/assets/images/WhatsApp Image 2024-06-06 at 12.35.11_bba1fca4.jpg" alt="" />
+      </div>
+      <div className="absolute top-0 h-[340px] justify-center items-start pl-[7vw] flex flex-col text-white">
+        <h1 className='text-6xl'>Save A Marriage Every Month</h1>
+        <h2 className='text-xl pt-[20px]'>Join 41,100 monthly donors with Social Impact Plan and start saving needy father every month</h2>
+        <button className='bg-white px-[2vw] py-[2vh] font-bold text-[#5487A0] mt-[25px]'>Start giving Monthly</button>
+      </div>
+      <div className='absolute top-[340px] flex flex-wrap'>
       <RequestCard/>
+      <RequestCard/>
+      <RequestCard/>
+      <RequestCard/>
+      <RequestCard/>
+      <RequestCard/>
+      </div>
+      
     </div>
   )
 }
